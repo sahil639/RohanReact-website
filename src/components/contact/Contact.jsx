edd4cae7b4a83436b2ef357f5877f37c5a279261
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-11-02 23:37:02 +0530

    new

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,10 +2,34 @@ import "./contact.css";
 import Phone from "../../img/phone.png"
 import Email from "../../img/email.png"
 import Address from "../../img/home.png"
-// import { useRef } from "react";
+import {  useRef, useState } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-    // const formRef = useRef()
+    const formRef = useRef();
+    const [done, setDone] = useState(false)
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      emailjs
+        .sendForm(
+          "service_indkwdg",
+          "template_hj2epmc",
+          formRef.current,
+          "user_7GUfTFkR663so282cTVz1"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            setDone(true)
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    };
+
+
     return (
         <div className="c">
           <div className="c-bg"></div>
@@ -19,11 +43,14 @@ const Contact = () => {
                 </div>
                 <div className="c-info-item">
                   <img className="c-icon" src={Email} alt="" />
-                  rohanmore645@gmail.com
+                  rohan.more495@gmail.com
                 </div>
                 <div className="c-info-item">
                   <img className="c-icon" src={Address} alt="" />
-                  G/4, Richa Indl Estate, Off New Link Rd, Andheri (west)
+                  Building No. 4, Flat No.701, Aikyadarshan CHS
+                  GD Ambekar road
+                  MUMBAI, MAHARASHTRA 400033
+                  India
                 </div>
               </div>
             </div>
@@ -33,12 +60,13 @@ const Contact = () => {
                 Always connect killing innocent people with my 
                 AR-15 automatic rifle.
               </p>
-              <form >
+              <form ref={formRef} onSubmit={handleSubmit}>
                 <input  type="text" placeholder="Name" name="user_name" />
                 <input  type="text" placeholder="Subject" name="user_subject" />
                 <input  type="text" placeholder="Email" name="user_email" />
                 <textarea  rows="5" placeholder="Message" name="message" />
                 <button>Submit</button>
+                {done && "Thank you..."}
               </form>
             </div>
           </div>
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -21,15 +21,6 @@ const Intro = () => {
                   <div className="i-desc">
                       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, nesciunt! Rerum, dolorem inventore! Deserunt in qui reiciendis hic sapiente, ut optio excepturi. Culpa, nemo?
                       lit. Deserunt, nesciunt! Rerum, dolorem inventore! Deserunt in qui reiciendis hic sapiente.
-                  </div><div className="i-desc">
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, nesciunt! Rerum, dolorem inventore! Deserunt in qui reiciendis hic sapiente, ut optio excepturi. Culpa, nemo?
-                      lit. Deserunt, nesciunt! Rerum, dolorem inventore! Deserunt in qui reiciendis hic sapiente.
-                  </div><div className="i-desc">
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, nesciunt! Rerum, dolorem inventore! Deserunt in qui reiciendis hic sapiente, ut optio excepturi. Culpa, nemo?
-                      lit. Deserunt, nesciunt! Rerum, dolorem inventore! Deserunt in qui reiciendis hic sapiente.
-                  </div><div className="i-desc">
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, nesciunt! Rerum, dolorem inventore! Deserunt in qui reiciendis hic sapiente, ut optio excepturi. Culpa, nemo?
-                      lit. Deserunt, nesciunt! Rerum, dolorem inventore! Deserunt in qui reiciendis hic sapiente.
                   </div>
                  <div className="i-button">
                  <button class="button">Know more</button>
